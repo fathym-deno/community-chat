0b7e832bd890fff289bbed2ea2b338f1cee2dd2c
GuerrillaGorilla <Guerrilla.Gorilla@outlook.com>
2023-09-15 00:37:38 -0400

    Line and Bar Chart on Index

--- routes/index.tsx
+++ routes/index.tsx
@@ -48,6 +48,29 @@ export default function Home(props: PageProps) {
             }],
           }}
         />
+        <Chart
+          type="bar"
+          options={{
+            devicePixelRatio: 1,
+            scales: { yAxes: [{ ticks: { beginAtZero: true } }] },
+          }}
+          data={{
+            labels: ["1", "2", "3"],
+            datasets: [{
+              label: "Sessions",
+              data: [123, 234, 234],
+              borderColor: ChartColors.Red,
+              backgroundColor: transparentize(ChartColors.Red, 0.5),
+              borderWidth: 1,
+            }, {
+              label: "Users",
+              data: [346, 233, 123],
+              borderColor: ChartColors.Blue,
+              backgroundColor: transparentize(ChartColors.Blue, 0.5),
+              borderWidth: 1,
+            }],
+          }}
+        />
       </div>
     </div>
   );
