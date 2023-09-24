7de05ddd0d53181bd6740ac44ae736e5539bbb35
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-09-24 17:20:51 +0000

    "Enhanced ChatInput functionality: Enter to submit, Shift+Enter for newline, manual form submission, and dynamic message state update."

--- islands/ChatInput.tsx
+++ islands/ChatInput.tsx
@@ -4,22 +4,42 @@ import moment from "npm:moment";
 import { UserIcon } from "../build/iconset/icons/UserIcon.tsx";
 import { LovebotIcon } from "../build/iconset/icons/LovebotIcon.tsx";
 import { SendIcon } from "$fathym/atomic-icons";
+import { useState } from 'react';
 
 interface ChatInputProps {
   postSrc: string;
 }
 
+export function ChatInput(props: ChatInputProps) {
+  const [message, setMessage] = useState('');
 
-export function
-  ChatInput(props: ChatInputProps) {
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(message);
+    setMessage('');
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      handleSubmit(event);
+    }
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(event.target.value);
+  };
 
   return (
-    <form method="post" src={props.postSrc} class="my-3 rounded-md p-3 bg-blue-600 bg-opacity-10 border border-blue-500 border-opacity-40">
+    <form method="post" src={props.postSrc} class="my-3 rounded-md p-3 bg-blue-600 bg-opacity-10 border border-blue-500 border-opacity-40" onSubmit={handleSubmit}>
       <div className="relative z-0 flex">
         <textarea
           name="content"
           className="block w-full rounded-sm rounded-r-none border-gray-300 text-sm shadow-sm focus:z-10 dark:bg-slate-950 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 text-black"
           placeholder="Ask Thinky..."
+          value={message}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
         ></textarea>
 
         <button
