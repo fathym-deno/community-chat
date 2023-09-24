f709bbd86135b3264f54d9b66326868a85d8cc7a
mcgear <michael.gearhardt@gmail.com>
2023-09-24 11:28:30 -0600

    Shift enter for new line, enter to submit

--- islands/ChatInput.tsx
+++ islands/ChatInput.tsx
@@ -1,44 +1,29 @@
-import { ConversationMessage } from "../state-flow/database.ts";
-import { render } from "$gfm";
-import moment from "npm:moment";
-import { UserIcon } from "../build/iconset/icons/UserIcon.tsx";
-import { LovebotIcon } from "../build/iconset/icons/LovebotIcon.tsx";
 import { SendIcon } from "$fathym/atomic-icons";
-import { useState } from 'react';
+import { JSX } from "preact";
+import { useState } from "preact/hooks";
 
 interface ChatInputProps {
   postSrc: string;
 }
 
 export function ChatInput(props: ChatInputProps) {
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    console.log(message);
-    setMessage('');
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent) => {
+  const handleKeyDown = (event: KeyboardEvent) => {
     if (event.key === 'Enter' && !event.shiftKey) {
       event.preventDefault();
-      handleSubmit(event);
+
+      const formEl: HTMLFormElement = document.querySelector('#chat-input')!;
+
+      formEl.submit();
     }
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(event.target.value);
-  };
-
   return (
-    <form method="post" src={props.postSrc} class="my-3 rounded-md p-3 bg-blue-600 bg-opacity-10 border border-blue-500 border-opacity-40" onSubmit={handleSubmit}>
+    <form id="chat-input" method="post" src={props.postSrc} class="my-3 rounded-md p-3 bg-blue-600 bg-opacity-10 border border-blue-500 border-opacity-40">
       <div className="relative z-0 flex">
         <textarea
           name="content"
           className="block w-full rounded-sm rounded-r-none border-gray-300 text-sm shadow-sm focus:z-10 dark:bg-slate-950 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 text-black"
-          placeholder="Ask Thinky..."
-          value={message}
-          onChange={handleChange}
+          placeholder="Ask Thinky... (Shift + Enter for a new line)"
           onKeyDown={handleKeyDown}
         ></textarea>
 
