061886a7a159338dcaafc3f70260ac89e4e000d1
michael.gearhardt@fathym.com <michael.gearhardt@fathym.com>
2023-09-28 18:55:25 +0000

    "Added 'Regenerate' and 'Save' buttons, 'lookup', 'name', and 'details' inputs to PortrayalForm.tsx, ensuring match with Portrayal class."

--- islands/PortrayalForm.tsx
+++ islands/PortrayalForm.tsx
@@ -16,21 +16,38 @@ export function PortrayalForm(props: PortrayalFormProps) {
       class="my-3 rounded-md p-3 bg-blue-600 bg-opacity-10 border border-blue-500 border-opacity-40"
     >
       <div className="relative z-0 flex">
-        <select
-          name="portrayal"
+        <input
+          type="text"
+          name="lookup"
+          placeholder="Enter lookup"
           className="block w-full rounded-sm rounded-r-none border-gray-300 text-sm shadow-sm focus:z-10 dark:bg-slate-950 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 text-black"
+        />
+
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          className="block w-full rounded-sm rounded-r-none border-gray-300 text-sm shadow-sm focus:z-10 dark:bg-slate-950 focus:border-primary-400 focus:ring focus:ring-primary-200 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 text-black"
+        />
+
+        <input
+          type="hidden"
+          name="details"
+          value="{}" // replace this with the actual portrayal details
+        />
+
+        <button
+          type="button"
+          className="flex items-center space-x-1 rounded-sm rounded-l-none border border-l-0 border-blue-600 bg-blue-600 px-3 py-1.5 text-center text-xs font-medium text-white shadow-sm transition-all hover:border-blue-800 hover:bg-blue-800 focus:ring focus:ring-blue-200 disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300"
         >
-          {props.options.map(option => {
-            return <option value={option.name}>{option.name}</option>;
-          })}
-        </select>
+          Regenerate
+        </button>
 
         <button
           type="submit"
-          for="version"
           className="flex items-center space-x-1 rounded-sm rounded-l-none border border-l-0 border-blue-600 bg-blue-600 px-3 py-1.5 text-center text-xs font-medium text-white shadow-sm transition-all hover:border-blue-800 hover:bg-blue-800 focus:ring focus:ring-blue-200 disabled:cursor-not-allowed disabled:border-blue-300 disabled:bg-blue-300"
         >
-          Submit
+          Save
         </button>
       </div>
     </form>
