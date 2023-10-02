d768ded9f8da2b9364e000c0ae7da5dc374ac924
mcgear <michael.gearhardt@gmail.com>
2023-10-02 06:20:29 -0600

    Cleanup file order

--- state-flow/database.ts
+++ state-flow/database.ts
@@ -7,14 +7,14 @@ import {
 import { AzureKeyCredential, OpenAIClient } from "npm:@azure/openai@next";
 import personalities from "./personalities.config.ts";
 
-const kv = await Deno.openKv();
-
-export const ConvoState: IConversationState = new DenoKVConversationState(kv);
-
 const endpoint = Deno.env.get("OPENAI_ENDPOINT") || "";
 const azureApiKey = Deno.env.get("OPENAI_API_KEY") || "";
 
+const kv = await Deno.openKv();
+
 const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+
+export const ConvoState: IConversationState = new DenoKVConversationState(kv);
 
 export const LLM = new OpenAILLMAccessor(client);
 
