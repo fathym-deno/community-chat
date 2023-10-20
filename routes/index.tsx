5a37a710fdf25f39f0ecc0c307a6e60707f4c566
mcgear <michael.gearhardt@gmail.com>
2023-10-20 09:15:16 -0600

    cleanup comments

--- routes/index.tsx
+++ routes/index.tsx
@@ -54,37 +54,6 @@ export default function Home(props: PageProps) {
         />
       </div>
 
-      {/* <form method="post" class="w-full max-w-sm py-8">
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              htmlFor="password"
-            >
-              Password
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500"
-              id="password"
-              type="password"
-              name="password"
-            />
-          </div>
-        </div>
-        <div class="md:flex md:items-center">
-          <div class="md:w-1/3"></div>
-          <div class="md:w-2/3">
-            <button
-              class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-              type="submit"
-            >
-              Login
-            </button>
-          </div>
-        </div>
-      </form> */}
     </div>
   );
 }
